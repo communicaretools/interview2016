75218b1defb10a8d89477acd95e0b779550e0312
Stein Jakob Nordbø <steinjak@gmail.com>
2016-06-02 21:57:37 +0200

    Added a simple function to populate the dev database

--- api/Gulpfile.js
+++ api/Gulpfile.js
@@ -16,3 +16,26 @@ gulp.task('test', function () {
             process.exit(0);
         });
 });
+
+gulp.task('populate-db', function (done) {
+    var db = require('./app/db');
+    var User = require('./app/users/User');
+    var Message = require('./app/messages/Message');
+
+    var users = [
+        new User({username: 'admin', name: 'Administrator'}),
+        new User({username: 'anna', name: 'Anna Karenina'}),
+        new User({username: 'don', name: 'Don Quixote'})
+    ];
+    users.forEach(u => u.setPassword('test'));
+
+    User.create(users, function (err, saved) {
+        var messages = [];
+        saved.forEach(function (user, index) {
+            messages.push({owner: user._id, from: saved[(index+1) % saved.length]._id, to: user._id, location: 'inbox', subject: 'One', body: 'Test message'}),
+            messages.push({owner: user._id, from: saved[(index+2) % saved.length]._id, to: user._id, location: 'inbox', subject: 'Two', body: 'Test message'}),
+            messages.push({owner: user._id, from: saved[(index+1) % saved.length]._id, to: user._id, location: 'inbox', subject: 'Three', body: 'Test message'})
+        });
+        Message.create(messages, done);
+    });
+});
